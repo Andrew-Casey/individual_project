1f1b0e331f6610ae63e70f3e729206c53d18e7f4
Andrew Casey <casey.andrew.m@gmail.com>
2023-06-07 09:04:29 -0500

    adding updates

---
+++ prepare.py
@@ -0,0 +1,185 @@
+import pandas as pd
+import numpy as np
+
+
+import wrangle as w
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def split_data(df, target):
+    '''
+    take in a DataFrame and target variable. return train, validate, and test DataFrames; stratify on target variable.
+    return train, validate, test DataFrames.
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[target])
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.25, 
+                                       random_state=123, 
+                                       stratify=train_validate[target])
+    return train, validate, test
+
+def scaled_df(train, validate, test):
+    """
+    This function scales the train, validate, and test data using the MinMaxScaler.
+
+    Parameters:
+    train (pandas DataFrame): The training data.
+    validate (pandas DataFrame): The validation data.
+    test (pandas DataFrame): The test data.
+
+    Returns:
+    Tuple of:
+        X_train_scaled (pandas DataFrame): The scaled training data.
+        X_validate_scaled (pandas DataFrame): The scaled validation data.
+        X_test_scaled (pandas DataFrame): The scaled test data.
+        y_train (pandas Series): The target variable for the training data.
+        y_validate (pandas Series): The target variable for the validation data.
+        y_test (pandas Series): The target variable for the test data.
+    """
+
+    X_train = train[['sg_putt_prev','sg_arg_prev','sg_app_prev','sg_ott_prev','sg_t2g_prev','sg_total_prev','driving_avg','fairways_hit','putting_avg']]
+    X_validate = validate[['sg_putt_prev','sg_arg_prev','sg_app_prev','sg_ott_prev','sg_t2g_prev','sg_total_prev','driving_avg','fairways_hit','putting_avg']]
+    X_test = test[['sg_putt_prev','sg_arg_prev','sg_app_prev','sg_ott_prev','sg_t2g_prev','sg_total_prev','driving_avg','fairways_hit','putting_avg']]
+
+    y_train = train.made_cut
+    y_validate = validate.made_cut
+    y_test = test.made_cut
+
+    #making our scaler
+    scaler = MinMaxScaler()
+    #fitting our scaler 
+    # AND!!!!
+    #using the scaler on train
+    X_train_scaled = scaler.fit_transform(X_train)
+    #using our scaler on validate
+    X_validate_scaled = scaler.transform(X_validate)
+    #using our scaler on test
+    X_test_scaled = scaler.transform(X_test)
+
+    # Convert the array to a DataFrame
+    X_train_scaled = pd.DataFrame(X_train_scaled, columns=X_train.columns, index=X_train.index)
+
+    # Convert the array to a DataFrame
+    X_validate_scaled = pd.DataFrame(X_validate_scaled, columns=X_validate.columns, index=X_validate.index)
+    
+    # Convert the array to a DataFrame
+    X_test_scaled = pd.DataFrame(X_test_scaled,columns=X_test.columns, index=X_test.index)
+    
+    return X_train_scaled, X_validate_scaled, X_test_scaled, y_train, y_validate, y_test
+
+#set summarize function
+def summarize(df):
+    '''
+    summarize will take in a single argument (a pandas dataframe) 
+    and output to console various statistics on said dataframe, including:
+    # .head()
+    # .info()
+    # .describe()
+    # .value_counts()
+    # observation of nulls in the dataframe
+    #distrinution of numerical attributes
+    '''
+    print(f"""SUMMARY REPORT
+=====================================================
+          
+          
+Dataframe head: 
+{df.head()}
+          
+=====================================================
+          
+          
+Dataframe info: """)
+    df.info()
+
+    print(f"""=====================================================
+          
+          
+Dataframe Description: 
+{df.describe().T}
+          
+=====================================================
+
+
+nulls in dataframe by column: 
+{nulls_by_col(df)}
+=====================================================
+
+
+nulls in dataframe by row: 
+{nulls_by_row(df)}
+=====================================================
+    
+    
+DataFrame value counts: 
+ """)         
+    for col in (get_object_cols(df)): 
+        print(f"""******** {col.upper()} - Value Counts:
+{df[col].value_counts()}
+    _______________________________________""")                   
+        
+# fig, axes = plt.subplots(1, len(get_numeric_cols(df)), figsize=(15, 5))
+    
+    for col in get_numeric_cols(df):
+        sns.histplot(df[col])
+        plt.title(f'Histogram of {col}')
+        plt.show()  
+
+def nulls_by_col(df):
+    """
+    This function will:
+        - take in a dataframe
+        - assign a variable to a Series of total row nulls for ea/column
+        - assign a variable to find the percent of rows w/nulls
+        - output a df of the two variables.
+    """
+    num_missing = df.isnull().sum()
+    pct_miss = (num_missing / df.shape[0]) * 100
+    cols_missing = pd.DataFrame({
+                    'num_rows_missing': num_missing,
+                    'percent_rows_missing': pct_miss
+                    })
+    
+    return  cols_missing
+
+def nulls_by_row(df):
+    """
+    This function will:
+        - take in a dataframe
+        - assign a variable to a Series of total row nulls for ea/column
+        - assign a variable to find the percent of rows w/nulls
+        - output a df of the two variables.
+    """
+    num_missing = df.isnull().sum(axis=1)
+    pct_miss = (num_missing / df.shape[1]) * 100
+    
+    rows_missing = pd.DataFrame({
+                    'num_cols_missing': num_missing,
+                    'percent_cols_missing': pct_miss
+                    })
+    
+    return rows_missing
+
+def get_object_cols(df):
+    '''
+    This function takes in a dataframe and identifies the columns that are object types
+    and returns a list of those column names. 
+    '''
+    # get a list of the column names that are objects (from the mask)
+    object_cols = df.select_dtypes(include=['object', 'category']).columns.tolist()
+    
+    return object_cols
+
+def get_numeric_cols(df):
+    '''
+    This function takes in a dataframe and identifies the columns that are object types
+    and returns a list of those column names. 
+    '''
+    # get a list of the column names that are objects (from the mask)
+    num_cols = df.select_dtypes(exclude=['object', 'category']).columns.tolist()
+    
+    return num_cols
+
